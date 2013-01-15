5a8b36360c49abbd69503fde87eacb48af068d72
John McLear <john@mclear.co.uk>
2013-01-15 18:12:45 +0000

    first commit

---
+++ noscript.js
@@ -0,0 +1,16 @@
+var eejs = require('ep_etherpad-lite/node/eejs/');
+var padManager = require('ep_etherpad-lite/node/db/PadManager');
+var ERR = require("ep_etherpad-lite/node_modules/async-stacktrace");
+
+exports.eejsBlock_body = function (hook_name, args, cb) {
+  padText = padManager.getPad(args.renderContext.req.params.pad, function(err, pad)
+  {
+    ERR(err);
+    args.content = args.content + "<noscript>"+safe_tags(pad.text())+"</noscript>";
+  });
+  return cb();
+}
+
+function safe_tags(str) {
+    return str.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;') ;
+}
