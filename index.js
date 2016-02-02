bcd3dffefa99be86a4511006f76a5320d6d3278d
John McLear <john@mclear.co.uk>
2016-02-02 10:25:51 -0800

    support for titles set by ep_Set_title_on_pad

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
  padManager = require('ep_etherpad-lite/node/db/PadManager'),
         ERR = require("ep_etherpad-lite/node_modules/async-stacktrace"),
       async = require('ep_etherpad-lite/node_modules/async'),
+         db = require('ep_etherpad-lite/node/db/DB').db,
     express = require('ep_etherpad-lite/node_modules/express');
+
+db['dbSettings'].cache = 0;
 
 exports.expressConfigure = function(hook_name, args, cb) {
 }
@@ -10,12 +13,19 @@ exports.expressConfigure = function(hook_name, args, cb) {
 exports.expressServer = function (hook_name, args, cb) {
   args.app.get('/public/:padId(*)', function(req, res) { 
     var padId = req.params.padId.replace(/\.\./g, '').split("?")[0];
+    var title;
+
+    // ep_set_title_on_pad exposes this database value, let's see if we have it.
+    db.get("title:"+padId, function(err, dbTitle){
+      if(dbTitle){
+        title = dbTitle;
+      }
+    });
 
     var pad;
     async.series([
       function(callback){ // Get the pad Text
         var padText = padManager.getPad(padId, function(err, _pad){
-           console.warn(_pad);
           pad = _pad;
           text = safe_tags(pad.text()).replace(/\n/g,"<br/>");
           ERR(err);
@@ -29,7 +39,9 @@ exports.expressServer = function (hook_name, args, cb) {
         it would bring in the .ejs content only and then on second load pad contents would be included..
         Sorry this is ugly but that's how the plugin FW was designed by @redhog -- bug him for a fix! */
 
-        args.content = "<html><body>";
+        args.content = "<html><head>";
+        if(title) args.content += "<title>"+title+"</title>";
+        args.content += "</head><body>";
         args.content += "<style>body{font-family:arial;font-size:14px;margin-top:20px;max-width:800px;margin-left:auto;margin-right:auto;}#editLink{float:right;font-weight:bold;margin-top:20px;}</style>";
         args.content += "<div id='padContents'>"+text+"</div>";
         args.content += "<div id='editLink'><a href='/p/"+padId+"'>Edit this pad</a></div>";
